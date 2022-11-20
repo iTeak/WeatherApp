d4e2afee21600ee6677035d71922cf243e172fe2
Prateek Verghese <prateek@2Prateeks-MacBook-Pro.local>
2022-11-19 23:42:22 -0800

    Startingover

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-var key = "1a420f2261150ceaac9bd836c340f2fe"
+let searchbutton = document.getElementById('')
+let inputvalue = document.getElementById('se')
+let temp = document.querySelector(".temp")
+let description = document.querySelector(".desc")
+let searchbar = document.getElementById('se')
+let key = "1a420f2261150ceaac9bd836c340f2fe"
+
+searchbutton.addEventListener("click")
+
+
+function getweather(){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&appid=${key}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+        })}
+
+
+
+
+
+
+
+
+
+
