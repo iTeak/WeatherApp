a9098e848c5abf5d27f578eeb34afe8165c0e2ac
Prateek Verghese <prateek@2Prateeks-MacBook-Pro.local>
2022-11-21 00:38:08 -0800

    Javaworks now...to make it display

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-let searchbutton = document.getElementById('')
-let inputvalue = document.getElementById('se')
-let temp = document.querySelector(".temp")
-let description = document.querySelector(".desc")
-let searchbar = document.getElementById('se')
-let key = "1a420f2261150ceaac9bd836c340f2fe"
-
-searchbutton.addEventListener("click")
+var key = "1a420f2261150ceaac9bd836c340f2fe"
+var searchbar = document.getElementById("search-bar")
+var button = document.getElementById('btn')
+var inputvalue = document.getElementById("search-bar")
 
 
-function getweather(){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&appid=${key}`)
+console.log(inputvalue.value)
+
+button.addEventListener("click", function getweather(){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&appid=${key}`)     
         .then((response) => response.json())
         .then((data) => {
             console.log(data)
-        })}
+            
+        })})
+
+
+
 
 
 
