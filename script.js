7bbdd5e504791d72989d2ebca4c043e846e79f3b
Prateek Verghese <prateek@2Prateeks-MacBook-Pro.local>
2022-11-24 18:18:48 -0800

    probably should have use java to dynamically build the cards

--- script.js
+++ script.js
@@ -7,13 +7,25 @@ var inputvalue = document.getElementById("search-bar")
 console.log(inputvalue.value)
 
 button.addEventListener("click", function getweather(){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&appid=${key}`)     
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&appid=${key}&units=imperial`)     
         .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-            
-        })})
+        // .then(data => console.log(data))
+        .then((data) => {const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        const{ lat , long } = data.coord;
 
+        document.querySelector(".city").innerText =  name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humid").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " mph";
+        console.log(data)
+        
+        })
+    })
 
 
 
